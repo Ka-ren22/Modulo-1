b8989a3e38202024600237ba186a282a180b70fd
Ka-ren22 <nahir.k.sosa@gmail.com>
2022-05-26 22:47:28 -0300

    Estructuras de datos II terminada

--- 04-EstructuraDeDatos-II/homework/homework.js
+++ 04-EstructuraDeDatos-II/homework/homework.js
@@ -10,7 +10,7 @@
 // remove():   Head --> null y devuelve 1
 // search: Busca un valor dentro de la lista. Puede recibir un valor o una función. Si no hubiera resultados, devuelve null.
 
-function LinkedList() {
+function LinkedList() {  //Estructura de la lista vacia
 
 	this._length = 0;
 	this.head = null;
@@ -18,45 +18,47 @@ function LinkedList() {
 
 function Node(value){
 	this.value = value;
-	this.next = null;
+	this.next = null;   //Me permite avanzar en la lista
 }
 
 let list = new LinkedList();
 
-LinkedList.prototype.add = function(value) {
-	let node = new Node(value);
-	let current = this.head;
-	if (!this.head) {
+LinkedList.prototype.add = function(value) {  //agrego al final
+	let node = new Node(value);    //Creo un nodo para agregar
+	let current = this.head;		//variable que guarde las referencias --> primero estoy en el head
+	if (!current) {					//si no hay head --> ubico el nuevo nodo
 		this.head = node;
 		return node;
 	} 
-		while(current.next){
-			current = current.next;
+		while(current.next){      //mientras exita nodo siguiente al next
+			current = current.next;  //avanzo
 		}
-		current.next = node;
-		return node;
+		current.next = node;		//me posiciono en el siguiente nodo
+ 		return node;
 };
 
-	list.add();
 
-LinkedList.prototype.remove = function(value) {
+LinkedList.prototype.remove = function(value) {   //remuevo al final
 
-
+let current = this.head;
 if(this.head === null){ //head vacio 
-		 return false;
+		 return null;
 	}
-	else if(this.head && !this.head.next){   //hay head pero no elemento siguiente
-		let node1 =this.head.value;
-		this.head = null;
-		return node1;
-	}
+	else if(this.head && !this.head.next){   //hay head pero no elemento siguiente --> un solo elemento
+		let aux = this.head.value;
+		this.head = null;  //elimino el head y me queda apuntando a null
+		this._length--;
+		return aux;
+	}								//	Para n elementos
 		while(current.next.next){ //Mientras haya elemento siguiente
-			current = current.next; //Me muevo al siguiente elemento
-			let node1 = current.next.value; //guardo la informacion en una variable antes de remover
-			current.next = null; //remuevo el ultimo elemento y queda en null
-			return node1;
+			current = current.next;	
 		}
-};
+		let aux1 = current.next.value; 
+		current.next = null; 
+		this._length--;
+		return aux1;
+};		
+
 
 LinkedList.prototype.search = function(value) {
 	if(this.head === null){   //si el head es null --> null
@@ -90,19 +92,22 @@ LinkedList.prototype.search = function(value) {
 
 function HashTable() {
 	this.numBuckets = 35;
+	this.buckets = [];
+
 }
 
 HashTable.prototype.hash= function(key){
 	let sum = 0;
-	for (let i = 0 ; i < key.length ; i++) {
-		sum += key.charCodeAt(i) //suma la cantidad de caracteres del key
+	for (let i = 0 ; i < key.length ; i++) {   //recorro el string
+		sum += key.charCodeAt(i); //devuelve la posicion del arreglo, tengo que indicar la posicion de la cadena, en este caso es(i)
 	}
 	return sum % this.numBuckets;  //retornar el resto de la suma divido el numero de Buckets
 };
 
 HashTable.prototype.set= function(key, value){
 	if (typeof key!== 'string') {
-		throw new typeError ("Keys must be string");
+		 throw new TypeError("Keys must be string");
+
 	}
 	let i = this.hash(key); //guardo el valor en la variables
 	if (this.buckets[i] === undefined) {
@@ -117,8 +122,14 @@ HashTable.prototype.get= function(key){
 };
 
 HashTable.prototype.hasKey= function(key){
+	
 	let i = this.hash(key);
-	return this.buckets[i].hasOwnProperty[key];
+	if (this.buckets[i]){
+		return this.buckets[i].hasOwnProperty(key);
+	} else{
+		return false;
+	}
+
 };
 
 
